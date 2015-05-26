f5ac21214a5e553f5783581e8c2e86f2732f4c80
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-05-26 20:51:19 +0200

    Initial commit.

---
+++ plugin.js
@@ -0,0 +1,139 @@
+const format = require("util").format;
+const inspect = require("util").inspect;
+const fetch = require("node-fetch");
+fetch.Promise = require("bluebird");
+
+function getIssueSummary (options) {
+    const user = options.user;
+    const repo = options.repo;
+    const issueNumber = options.issue;
+
+    return fetch(format("https://api.github.com/repos/%s/%s/issues/%s", user, repo, issueNumber))
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (res) {
+        if (res.message === "Not Found") {
+            return "Issue does not exist.";
+        }
+
+        const type = res.pull_request ? "PR" : "Issue";
+        const status = res.state;
+        const title = res.title;
+        const link = res.html_url;
+
+        return format("[%s %s] <%s> %s (%s)", type, issueNumber, status, title, link);
+    });
+}
+
+function withIssue (obj, issueNumber) {
+    obj.issue = issueNumber;
+    return obj;
+}
+
+function stringIsPositiveInteger (string) {
+    const number = Number(string);
+
+    if (isNaN(number)) {
+        return false;
+    }
+
+    if (number === Infinity) {
+        return false;
+    }
+
+    if (number < 1) {
+        return false;
+    }
+
+    if (Math.floor(number) !== number) {
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = {
+    name: "github",
+    init: function (client, deps) {
+        const ghUser = client.config("github-user");
+        if (!ghUser) { throw new Error("github-user configuration value must be set."); }
+        const ghRepo = client.config("github-repo");
+        if (!ghRepo) { throw new Error("github-repo configuration value must be set."); }
+
+        /// Takes a string of one of the following formats and turns it into
+        /// a {user, repo} object defaulting to the client's config values
+        /// for missing values.
+        ///
+        /// ""
+        /// "repo"
+        /// "user/repo"
+        /// "user/"
+        /// "/repo"
+        /// "/"
+        function parseUserRepoString (userRepoString) {
+            if (userRepoString.length === 0) {
+                return {user: ghUser, repo: ghRepo};
+            }
+
+            if (userRepoString.indexOf("/") === -1) {
+                return {user: ghUser, repo: userRepoString};
+            }
+
+            const split = userRepoString.split("/");
+            const user = split[0].length !== 0 ? split[0] : ghUser;
+            const repo = split[1].length !== 0 ? split[1] : ghRepo;
+
+            return {user: user, repo: repo};
+        }
+
+        return {
+            handlers: {
+                "!gh": function (command) {
+                    // There's a lot of missing error handling here.
+                    // e.g., the Number testing ignores the possibility of NaN.
+
+                    if (command.args.length === 0) {
+                        // !gh
+                        return format("https://github.com/%s/%s", ghUser, ghRepo);
+                    }
+
+                    if (command.args.length === 1) {
+                        const arg = command.args[0];
+
+                        if (stringIsPositiveInteger(arg)) {
+                            // !gh Number
+                            return getIssueSummary({user: ghUser, repo: ghRepo, issue: arg});
+                        } else {
+                            const userRepo = parseUserRepoString(arg);
+                            return format("https://github.com/%s/%s", userRepo.user, userRepo.repo);
+                        }
+                    }
+
+                    if (command.args.length === 2) {
+                        if (stringIsPositiveInteger(command.args[1])) {
+                            return getIssueSummary(withIssue(parseUserRepoString(command.args[0]), command.args[1]));
+                        } else {
+                            return format("https://github.com/%s/%s", command.args[0], command.args[1]);
+                        }
+                    }
+
+                    if (command.args.length === 3) {
+                        return getIssueSummary({
+                            user: command.args[0],
+                            repo: command.args[1],
+                            issue: command.args[2]
+                        });
+                    }
+
+                    return "!gh [user/repo] issue-number";
+                }
+            },
+
+            commands: ["gh"],
+            help: {
+                "gh": ["!gh [user/repo] issue-number"]
+            }
+        };
+    }
+};
---
+++ readme.md
@@ -0,0 +1,5 @@
+![Travis Status](https://img.shields.io/travis/Havvy/tennu-github.svg) ![NPM Downloads](https://img.shields.io/npm/dm/tennu-github.svg) ![Version](https://img.shields.io/npm/v/tennu-github.svg) ![ISC Licensed](https://img.shields.io/npm/l/tennu-github.svg) ![Github Issue Count](https://img.shields.io/github/issues/Havvy/tennu-github.svg) ![Github Stars](https://img.shields.io/github/stars/Havvy/tennu-github.svg)
+
+Rust help plugin for the Tennu IRC Bot framework.
+
+See [here](https://tennu.github.io/plugins/github) for documentation.
